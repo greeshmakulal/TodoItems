190afea9c169800a48f090e1e0ae8da6a1f78a4a
Greeshma <86409283+greeshmakulal@users.noreply.github.com>
2023-11-23 18:11:24 +0530

    Update MainActivity.kt

--- app/src/main/java/com/simplequiz/todoitems/MainActivity.kt
+++ app/src/main/java/com/simplequiz/todoitems/MainActivity.kt
@@ -22,34 +22,9 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var todoAdapter:TodoAdapter
-//    lateinit var wifiSwitch: Switch
-//    lateinit var wifiManager: WifiManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val intent = Intent(this, Myservice::class.java)
-//        btn_start.setOnClickListener {
-//            startService(intent)
-//        }
-//        btn_stop.setOnClickListener {
-////            stopService(intent)
-//
-//
-//            CoroutineScope(IO).launch {
-//                fakrequestApi()
-//            }
-//        }
-
-//        wifiSwitch = findViewById(R.id.wifiSwitch)
-//        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-//        wifiSwitch.setOnCheckedChangeListener { _, isChecked ->
-//            if (isChecked) {
-//                wifiManager.isWifiEnabled= true
-//                wifiSwitch.text = "WiFi is ON"
-//            } else {
-//                wifiManager.isWifiEnabled = false
-//                wifiSwitch.text = "WiFi is OFF"
-//            }
 
             todoAdapter = TodoAdapter(mutableListOf())
             rvTodoItems.adapter = todoAdapter
@@ -69,31 +44,3 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-//    override fun onStart() {
-//        super.onStart()
-////        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
-////        registerReceiver(wifiStateReceiver, intentFilter)
-//    }
-//    override fun onStop() {
-//        super.onStop()
-////        unregisterReceiver(wifiStateReceiver)
-//    }
-//    private val wifiStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-//        override fun onReceive(context: Context, intent: Intent) {
-//            when (intent.getIntExtra(
-//                WifiManager.EXTRA_WIFI_STATE,
-//                WifiManager.WIFI_STATE_UNKNOWN
-//            )) {
-//                WifiManager.WIFI_STATE_ENABLED -> {
-//                    wifiSwitch.isChecked = true
-//                    wifiSwitch.text = "WiFi is ON"
-//                    Toast.makeText(this@MainActivity, "Wifi is On", Toast.LENGTH_SHORT).show()
-//                }
-//                WifiManager.WIFI_STATE_DISABLED -> {
-//                    wifiSwitch.isChecked = false
-//                    wifiSwitch.text = "WiFi is OFF"
-//                    Toast.makeText(this@MainActivity, "Wifi is Off", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//    } }
